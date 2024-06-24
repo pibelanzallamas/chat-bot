faff208dfe466c7c80480e48c873090d974a6f03
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-06-24 15:06:57 -0300

    history save it correctly

--- app/page.js
+++ app/page.js
@@ -1,71 +1,53 @@
 "use client";
 
+import axios from "axios";
 import { useRef, useState, useEffect } from "react";
-import NLPCloudClient from "nlpcloud";
 import "./page.css";
 
 export default function Home() {
+  const containerRef = useRef(null);
   const [query, setQuery] = useState("");
   const [user, setUser] = useState([
     "hola, que tal?",
     "bien bien, que tranzas?",
   ]);
   const [jojo, setJojo] = useState(["bien, y tu?", "nada ahi, agitandole"]);
-
-  const client = new NLPCloudClient({
-    model: "dolphin-yi-34b",
-    token: "",
-    gpu: true,
-  });
-
   const context =
     "This is a discussion between a human and an AI. The human is happy but curious and the AI is empathetic and helpful. The AI is called Jojo.";
-  const history = [];
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    if (containerRef.current) {
-      containerRef.current.scrollTop = containerRef.current.scrollHeight;
-    }
-  }, [user, jojo]);
+  const [history, setHistory] = useState([
+    { input: "hola, que tal?", response: "bien, y tu?" },
+    { input: "bien bien, que tranzas?", response: "nada ahi, agitandole" },
+  ]);
 
   function handleSubmit(e) {
     e.preventDefault();
     setUser([...user, query]);
 
-    client
-      .chatbot({
+    axios
+      .post("http://localhost:5000/api/nlpcloud", {
+        context,
+        history,
         input: query,
-        context: context,
-        history: history,
       })
-      .then(function (response) {
-        history.push({
-          input: query,
-          response: response.data.response,
-        });
-        setJojo([...jojo, response.data.response]);
+      .then((resp) => {
+        const response = resp.data;
+
+        setHistory(response.history);
+        setJojo((elemento) => [...elemento, response.response]);
       })
-      .catch(function (err) {
-        console.error(err.response.status);
-        console.error(err.response.data.detail);
+      .catch((err) => {
+        console.log(err);
       });
+
     setQuery("");
   }
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const result = await axios.post("http://localhost:5000/api/nlpcloud", {
-  //       input: query,
-  //       context,
-  //       history,
-  //     });
-  //     setResponse(result.data);
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // };
+  //para seguir la conver con el scroll
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [user, jojo]);
 
   return (
     <main>
